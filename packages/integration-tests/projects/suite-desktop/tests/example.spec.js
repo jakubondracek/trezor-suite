7dfefbaa6aedec85d9780a6c558b06006137b9a2
mroz <mvarmuza@gmail.com>
2021-11-22 17:02:22 +0100

    test

--- packages/integration-tests/projects/suite-desktop/tests/example.spec.js
+++ packages/integration-tests/projects/suite-desktop/tests/example.spec.js
@@ -5,14 +5,11 @@ let window, electronApp;
 
 test.beforeAll(async () => {
   // Launch Electron app.
-  // electronApp = await electron.launch({ args: ['../suite-desktop/dist/app.js'] });
-  // electronApp = await electron.launch({ args: ['../suite-desktop/build/js/main.js'] });
 
   electronApp = await electron.launch({
     cwd: '../suite-desktop',
-    args: ['.']
+    args: ['./dist/app.js']
   });
-
 
   // Evaluation expression in the Electron context.
   const appPath = await electronApp.evaluate(async ({ app }) => {
@@ -21,28 +18,19 @@ test.beforeAll(async () => {
     return app.getAppPath();
   });
 
-
-  // Get the first window that the app opens, wait if necessary.
-  // Print the title.
-  // console.log(await window.title());
-  // Capture a screenshot.
-  // await window.screenshot({ path: 'tests/screenshots/out.png' });
-  // Direct Electron console to Node terminal.
-  // window.on('console', console.log);
-
 });
 
 test('window has correct title', async () => {
-  await new Promise((resolve) => setTimeout(() => { resolve() }, 10000))
+  // await new Promise((resolve) => setTimeout(() => { resolve() }, 10000))
   window = await electronApp.firstWindow();
 
   const title = await window.title()
-  expect(title).toBe('Hello World!')
+  expect(title).toBe('Trezor Suite')
 })
 
 test.afterAll(async () => {
   // Exit app.
-  // await electronApp.close();
+  await electronApp.close();
 });
 
 // 
